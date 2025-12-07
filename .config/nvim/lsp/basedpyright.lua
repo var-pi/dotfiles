42d2c188349481457b7ad42ad1b6ba9241eb512c
Stefan Ehin <var-pi@vortex.lan>
2025-12-07 09:47:44 +0200

    Clean basedpyright.lua

--- .config/nvim/lsp/basedpyright.lua
+++ .config/nvim/lsp/basedpyright.lua
@@ -1,6 +1,3 @@
 return {
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
-    on_attach = function(client, bufnr)
-        local opts = { noremap = true, silent = true }
-    end,
 }
