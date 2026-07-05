ccd1e1d1fef68e7f5b983d450e34beefbfcfd27c
Stefan Ehin <stefanehin4@gmail.com>
2026-07-05 14:44:21 +0200

    Before changing neovim source

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -1,9 +1,9 @@
 {
     description = "vortex nix-darwin system flake";
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
         nix-darwin = {
-            url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+            url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
             inputs.nixpkgs.follows = "nixpkgs";
         };
         neovim-nightly-overlay = {
@@ -30,8 +30,9 @@
                         texlab
                         ripgrep # For telescope live_grep
                         python314
+                        claude-code
                         (writeShellScriptBin "julia-1.12" ''exec ${julia-bin}/bin/Julia "$@"'')
-                        julia_111-bin # For LanguageServer.jl
+                        julia-lts-bin # For LanguageServer.jl
                         nodejs_24 # For tree-sitter
                         tree-sitter # For nvim-treesitter
                         texliveFull
@@ -46,6 +47,7 @@
                     system.stateVersion = 6;
                     nixpkgs.hostPlatform = "aarch64-darwin";
                     security.pam.services.sudo_local.touchIdAuth = true;
+                    nixpkgs.config.allowUnfree = true;
                 };
         in
             {
